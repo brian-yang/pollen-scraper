66699b81c36a5c2fa0eef2453613982b58d57a75
Brian Yang <brian.yang328@gmail.com>
2016-06-30 19:54:10 -0400

    Scrape all data from website for bug fixing

--- scraper_app/spiders/pollen_spider.py
+++ scraper_app/spiders/pollen_spider.py
@@ -9,34 +9,19 @@ from scraper_app.items import PollenScraper
 # For debugging
 from scrapy import log
 
-
 class PollenSpider(Spider):
     name = 'pollenscraper'
 
     allowed_domains = ['pollen.com']
     start_urls = ['http://pollen.com/forecast/current/pollen/90210']
 
-    forecast_dates_xpath = '//div[@class="forecast-day"]'
+    forecast_dates_xpath = '//script[@id="/forecast/day.html"]'
     item_fields = {
-        'title': './/div[@class="chart-col-wrapper"]/div[@class="chart-col"]/div[@class="no-padding"]/h3[@class="day-header"]/text()',
-        'pollen_value': './/p[@class="forecast-level"]/text()',
-        'severity': './/p[@class="forecast-level-desc"]/text()'
+        'title': './/text()',
+        #'pollen_value': './/p[text()]',
+        #'severity': './/p[@class="forecast-level-desc"][text()]'
         #'top_allergens': '//span[@class="ng-binding"]/text()'
     }
 
-    def parse(self, response):
-        selector = Selector(response)
-        log.msg("CHECKPOINT #1")
-        log.msg(str(selector.xpath(self.forecast_dates_xpath)))
-
-        for forecast in selector.xpath(self.forecast_dates_xpath):
-            log.msg("CHECKPOINT #2")
-            loader = ItemLoader(PollenScraper(), selector=forecast)
-
-            loader.default_input_processor = MapCompose(unicode.strip)
-            loader.default_output_processor = Join()
-
-            for field, xpath in self.item_fields.iteritems():
-                log.msg("CHECKPOINT #3")
-                loader.add_xpath(field, xpath)
-                yield loader.load_item()
+    def parse_item(self, response):
+        pass
